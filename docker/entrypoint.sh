e6af897ef983a8154c82f655bbc0aabab5ad7fe7
Lawrence Bisnar <labisnar21@gmail.com>
2025-12-01 18:59:10 +0800

    Add Docker configuration for Render deployment

---
+++ docker/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Run migrations
+php artisan migrate --force
+
+# Cache configuration
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Start supervisor
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
