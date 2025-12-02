edcd3f77a2e28336080d897f77e327e68d473280
Lawrence Bisnar <labisnar21@gmail.com>
2025-12-02 12:33:09 +0800

    Switch back to Gmail SMTP (port 587/TLS)

--- config/mail.php
+++ config/mail.php
@@ -8,7 +8,7 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'default' => env('MAIL_MAILER', 'resend'),
+    'default' => env('MAIL_MAILER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
@@ -21,8 +21,8 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-            'port' => env('MAIL_PORT', 465),
-            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME', 'labisnar21@gmail.com'),
             'password' => env('MAIL_PASSWORD', 'fiwntdkpqmprjlmp'),
             'timeout' => env('MAIL_TIMEOUT', 30),
